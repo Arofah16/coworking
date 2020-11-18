176c269250b6dd318013cf58853abbe72d303899
arofah16 <arofahr06@gmail.com>
2020-11-18 17:45:36 +0700

    app bar

---
+++ src/components/Section1.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <v-img src="@/assets/room.jpg"></v-img>
+        <v-row style="align-items: center; height: 100%;">
+            <v-col cols="12" offside-md="5">
+                <h1 style="font-size: 3em;" class="text--white">
+                    Reimagine Your <br> Workspace
+                </h1>
+                <p class="text-white">
+                    Whether you’re an established enterprise <br />
+                    or a growing startup, discover flexible <br />
+                    spaces and solutions to move <br />
+                    your business forward.
+                </p>
+            </v-col>
+        </v-row>
+
+    </section>
+
+</template>
