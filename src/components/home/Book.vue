d6b1d92bfd8b215396ea3846261518809765bcd8
arofah16 <arofahr06@gmail.com>
2020-12-16 17:51:47 +0700

    modif

--- src/components/home/Book.vue
+++ src/components/home/Book.vue
@@ -1,25 +1,3 @@
-<!-- <template>
-    <v-form>
-        <v-container>
-            <v-row class="mt-15">
-                <v-col cols="12" sm="6" md="3" >
-                    <v-card  class="mx-auto my-auto"  justify="center"  outlined>
-                        <v-text-field label="nama" placeholder="Masukkan Nama" outlined>
-                        </v-text-field>
-                        <v-text-field label="No. HP" placeholder="Masukkan nomer hp" outlined>
-                        </v-text-field>
-                        <v-text-field label="No. Email" placeholder="Masukkan Email" outlined>
-                        </v-text-field>
-                        <v-text-field label="Tanggal Booking" placeholder="dd/mm/yyyy" outlined>
-                        </v-text-field>
-                        <v-btn color="green">SIMPAN</v-btn>
-                    </v-card>
-                </v-col>
-
-            </v-row>
-        </v-container>
-    </v-form>
-</template> -->
 <template>
     <div class="text-center">
       <v-dialog
